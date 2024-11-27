ea4cbfd4dda0b8b5f7fcba25e14d7cc566401c44
son <handsun000@gmail.com>
2024-11-27 10:17:37 +0900

    (3 + 5) * 5 BLUE

--- src/test/java/com/ll/Calc.java
+++ src/test/java/com/ll/Calc.java
@@ -2,9 +2,9 @@ package com.ll;
 
 public class Calc {
     public static int run1(String expr) {
-        if ("10 + 5".equals(expr)) {
-            return 15;
-        }
+//        if ("10 + 5".equals(expr)) {
+//            return 15;
+//        }
 
         String[] exprBits = expr.split(" \\+ ");
 
@@ -14,8 +14,14 @@ public class Calc {
         return num1+num2;
     }
     public static int run2(String expr) {
-        if ("(10 + 5) * 5".equals(expr)) return 75;
+//        if ("(10 + 5) * 5".equals(expr)) return 75;
+        String[] exprBits1 = expr.split(" \\* ");
+        String[] exprBits2 = exprBits1[0].replaceAll("[()]","").split(" \\+ ");
 
-        return 40;
+        int num1 = Integer.parseInt(exprBits2[0]);
+        int num2 = Integer.parseInt(exprBits2[1]);
+        int num3 = Integer.parseInt(exprBits2[1]);
+
+        return (num1+num2)*num3;
     }
 }
