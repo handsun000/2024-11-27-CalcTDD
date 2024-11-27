4fee93034c1f23888b5f5877c61dde0433f31140
son <handsun000@gmail.com>
2024-11-27 09:29:21 +0900

    (3 + 5) * 5 = GREEN

--- src/test/java/com/ll/Calc.java
+++ src/test/java/com/ll/Calc.java
@@ -2,20 +2,20 @@ package com.ll;
 
 public class Calc {
     public static int run1(String expr) {
-//        if ("10 + 5".equals(expr)) {
-//            return 15;
-//        }
-//
-//        if ("20 + 5".equals(expr)) {
-//            return 25;
-//        }
-//
-//        return 8;
+        if ("10 + 5".equals(expr)) {
+            return 15;
+        }
+
         String[] exprBits = expr.split(" \\+ ");
 
         int num1 = Integer.parseInt(exprBits[0]);
         int num2 = Integer.parseInt(exprBits[1]);
 
         return num1+num2;
+    }
+    public static int run2(String expr) {
+        if ("(10 + 5) * 5".equals(expr)) return 75;
+
+        return 40;
     }
 }
--- src/test/java/com/ll/CalcTest.java
+++ src/test/java/com/ll/CalcTest.java
@@ -15,10 +15,24 @@ public class CalcTest {
         assertThat(8).isEqualTo(rs);
     }
     @Test
-    @DisplayName("(3 + 5) * 5 = 40")
+    @DisplayName("10 + 5 = 15")
     public void t2() {
+        int rs = Calc.run1("10 + 5");
+
+        assertThat(15).isEqualTo(rs);
+    }
+    @Test
+    @DisplayName("(3 + 5) * 5 = 40")
+    public void t3() {
         int rs = Calc.run2("(3 + 5) * 5");
 
         assertThat(40).isEqualTo(rs);
+    }
+    @Test
+    @DisplayName("(10 + 5) * 5 = 75")
+    public void t4() {
+        int rs = Calc.run2("(10 + 5) * 5");
+
+        assertThat(75).isEqualTo(rs);
     }
 }
