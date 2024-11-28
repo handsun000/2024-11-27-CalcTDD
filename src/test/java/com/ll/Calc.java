dcc7614c6e92c48a4cd79d14defda0cb049666a6
son <handsun000@gmail.com>
2024-11-28 11:51:58 +0900

    recursive function

--- src/test/java/com/ll/Calc.java
+++ src/test/java/com/ll/Calc.java
@@ -19,17 +19,6 @@ public class Calc {
     }
 
     public static int run2(String expr) {
-//        if ("(10 + 5) * 5".equals(expr)) return 75;
-//        if ("5 * (3 + 5)".equals(expr)) return 40;
-//        String[] exprBits1 = expr.split(" \\* ");
-//        String[] exprBits2 = exprBits1[0].replaceAll("[()]","").split(" \\+ ");
-//
-//        int num1 = Integer.parseInt(exprBits2[0]);
-//        int num2 = Integer.parseInt(exprBits2[1]);
-//        int num3 = Integer.parseInt(exprBits1[1]);
-//        if ("(3 + 5) * 5 + -10".equals(expr)) return 30;
-//        if("-10 + 5 * (3 + 5)".equals(expr)) return 30;
-
         List<Character> operators = new ArrayList<>();
         List<Integer> numbers = new ArrayList<>();
         Stack<Integer> index = new Stack<>();
@@ -65,7 +54,7 @@ public class Calc {
                     numbers.add(Integer.parseInt(number.toString()));
                     number = new StringBuilder();
                 }
-                machine(operators, numbers, index.pop());
+                machine2(operators, numbers, index.pop());
 
             } else {
                 number.append(e);
@@ -76,9 +65,24 @@ public class Calc {
             }
         }
 
-        machine(operators, numbers, 0);
+        machine2(operators, numbers, 0);
 
         return numbers.removeLast();
+    }
+
+    private static void machine2(List<Character> operators, List<Integer> numbers, int start) {
+        if (start >= operators.size()) return;
+        if (operators.get(start) == '*' || operators.get(start) == '/') {
+            numbers.add(start, calculator(numbers.remove(start), numbers.remove(start), operators.remove(start)));
+            machine2(operators, numbers, start);
+        }
+        else {
+            machine2(operators, numbers, start+1);
+            if (start < operators.size()) {
+                numbers.add(start, calculator(numbers.remove(start), numbers.remove(start), operators.remove(start)));
+                machine2(operators, numbers, start);
+            }
+        }
     }
 
     private static void machine(List<Character> operators, List<Integer> numbers, int start) {
